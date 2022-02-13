2a488041e956fa57e785eedfe20a1a28d203619c
StephanYorchenko <stepan33314@yandex.ru>
2022-02-14 00:19:34 +0500

    слегонца порефакторил

---
+++ src/app/internal/transport/middlewares.py
@@ -0,0 +1,47 @@
+import functools
+from typing import List
+
+from django.http import JsonResponse, HttpResponse
+
+
+def get_only(func):
+    @functools.wraps(func.__name__)
+    def wrapper(request):
+        if request.method == 'GET':
+            return func(request)
+        return HttpResponse(f"Not right type of response", status=405)
+
+    return wrapper
+
+
+def handle_empty(func):
+    @functools.wraps(func.__name__)
+    def wrapper(request):
+        response = func(request)
+        if response is not None:
+            return func(request)
+        return HttpResponse(f'Does not exist', status=403)
+
+    return wrapper
+
+
+def to_json(func):
+    @functools.wraps(func.__name__)
+    def wrapper(request):
+        response = func(request)
+        if response is not None:
+            return JsonResponse(data=response.__dict__(), status=200)
+
+    return wrapper
+
+
+def parse_query_param(param_names: List[str]):
+    def decorator(func):
+        @functools.wraps(func.__name__)
+        def wrapper(request):
+            kwargs = {param_name: request.GET.get(param_name) for param_name in param_names}
+            return func(**kwargs)
+
+        return wrapper
+
+    return decorator
